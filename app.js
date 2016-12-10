be337f9464d7953028bcd2c7e3ebb5e21d629c34
Lord Chika Onyebueke <onye108@gmail.com>
2016-12-10 10:57:24 -0800

    added HTML CSS and button

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ function Stores (name, minCust, maxCust, avgCookieSales){
 
     }
   };
+
   this.calcCookieSales();
 
   this.render = function (){
@@ -40,6 +41,7 @@ function Stores (name, minCust, maxCust, avgCookieSales){
     storeList.textContent = name;
     storesTr.appendChild(storeList);
     table.appendChild(storesTr);
+
 //***************writing data in cells***************************
     for(var i = 0; i < hoursOpen.length; i++){
 
@@ -55,16 +57,18 @@ function Stores (name, minCust, maxCust, avgCookieSales){
   };
   this.render();
   myStores.push(this);
+
 }; //*******************END OF Constructor*********************************
 
+makeHeaderRows();
 
 console.log(this.randomNumArray);
 console.log(myStores);
 new Stores ('Alki', 2, 26, 2.6);
-new Stores ('Capitol Hill', 20, 38, 2.3);
-new Stores ('Sea Center', 11, 38, 3.7);
+new Stores ('CapHill', 20, 38, 2.3);
+new Stores ('SeaCenter', 11, 38, 3.7);
 new Stores ('SeaTacAP', 3, 24, 1.2);
-new Stores ('First and Pike', 23, 65, 6.3);
+new Stores ('FirstandPike', 23, 65, 6.3);
 
 function makeHeaderRows() {
 
@@ -89,7 +93,26 @@ function makeHeaderRows() {
   storeList.textContent = name;
   table.appendChild(storeList);
 };
-//****************Start Data Form **********************
+//*******************start footer****************************
+// function makeTotalrow() {
+//   var cookieStands = Stores[i].cookiesSold[k];
+//
+//   for (var i = 0; i < hoursOpen.length; i++) {
+//
+//     var totalCookies = 0;
+//   }
+//
+//   for (var j = 0; j < Stores[j].length; j++) {
+//
+//
+//       totalCookies += Stores[j].cookiesSold[i];
+//
+// }
+//
+//
+// };
+// //********************end footer******************************
+// //****************Start Data Form **********************
 var formData = document.getElementById('userinput');
 
 formData.addEventListener('submit', inputNewUserData);
@@ -97,17 +120,19 @@ formData.addEventListener('submit', inputNewUserData);
 function inputNewUserData (event) {
   event.preventDefault();
 
+//***********************form data regenrator***************************
+  var valueForName = event.target.elements.name.value;
+  var valueForMinCust = event.target.elements.minCust.value;
+  var valueForMaxCust = event.target.elements.maxCust.value;
+  var valueForavgCookieSales = event.target.elements.avgCookieSales.value;
+  console.log(valueForName);
+  var newLocation = new Stores (valueForName,valueForMinCust, valueForMaxCust, valueForavgCookieSales );
+  table.innerHTML = '';
 
-var valueForName = event.target.elements.name.value;
-var valueForMinCust = event.target.elements.minCust.value;
-var valueForMaxCust = event.target.elements.maxCust.value;
-var valueForavgCookieSales = event.target.elements.avgCookieSales.value;
-console.log(valueForName);
-var newLocation = new Stores (valueForName,valueForMinCust, valueForMaxCust, valueForavgCookieSales );
-table.innerHTML= '';
-makeHeaderRows();
+  // makeHeaderRows();
 
-for (var i = 0; i < myStores.length; i++) {
-  myStores[i].render();
-}
+
+  for (var i = 0; i < myStores.length; i++) {
+    myStores[i].render();
+  }
 };//***************end of inputNewUserData ******************
