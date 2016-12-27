b7b6a837cc75c3e6e18d0e19b0048fe0d30462c6
Kevia <cloud@kevia.me>
2016-12-27 13:45:45 -0800

    Start up hall stuff

--- app.js
+++ app.js
@@ -3,10 +3,13 @@
 var hoursOpen = ['6am','7am','8am','9am', '10am', '11am', '12pm','1pm', '2pm','3pm','4pm','5pm','6pm','7pm'];
 
 var myStores = [];
-var table = document.getElementById('table');
+var table = $('#table');
+var formData = document.getElementById('userinput');
+var inputs = $('input').first();
 
 
-function Stores (name, minCust, maxCust, avgCookieSales){
+
+function Stores(name, minCust, maxCust, avgCookieSales) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -15,83 +18,71 @@ function Stores (name, minCust, maxCust, avgCookieSales){
   this.randomNumArray = [];
   this.totalCookieSales = 0;
 
-  this.calcRandomNumber = function (){
-    for(var i = 0; i < hoursOpen.length; i++){
-      this.randomNumArray.push(Math.floor( Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-
-    }
-  };
-
-  this.calcCookieSales = function (){
-    this.calcRandomNumber();
-    for(var i = 0; i < hoursOpen.length; i++){
-      this.cookiesSold.push(Math.ceil(this.randomNumArray[i] * this.avgCookieSale));
-      this.totalCookieSales += this.cookiesSold[i];
-
-    }
-  };
-
   this.calcCookieSales();
-
-  this.render = function (){
-
-    var storesTr = document.createElement('tr');
-
-    var storeList = document.createElement('tr');
-    storeList.textContent = name;
-    storesTr.appendChild(storeList);
-    table.appendChild(storesTr);
-
-//***************writing data in cells***************************
-    for(var i = 0; i < hoursOpen.length; i++){
-
-      var hourlyDataCells = document.createElement('td');
-      hourlyDataCells.textContent = this.cookiesSold[i];
-      storesTr.appendChild(hourlyDataCells);
-    }
-    //Store names left side of table***************************
-    var lastColumnTotals = document.createElement('tr');
-    lastColumnTotals.textContent = this.totalCookieSales;
-    storesTr.appendChild(lastColumnTotals);
-
-  };
-  this.render();
   myStores.push(this);
+}
 
-}; //*******************END OF Constructor*********************************
+function buildRow(element, payload, blah) {
 
+  var tableRow = $('<tr></tr>');
+  var tableData = $(element);
+  tableData.text(blah);
+  tableRow.append(tableData);
 
-makeHeaderRows();
+  for(var i = 0; i < payload.length; i++){
 
-console.log(this.randomNumArray);
-console.log(myStores);
-new Stores ('Alki', 2, 26, 2.6);
-new Stores ('CapHill', 20, 38, 2.3);
-new Stores ('SeaCenter', 11, 38, 3.7);
-new Stores ('SeaTacAP', 3, 24, 1.2);
-new Stores ('FirstandPike', 23, 65, 6.3);
+    tableData = $(element);
+    tableData.text(payload[i]);
+    tableRow.append(tableData);
+  }
 
-function makeHeaderRows() {
-  var elementRow = document.createElement('tr');
+  tableData = $(element);
+  tableData.text('Totals');
+  tableRow.append(tableData);
 
-  var headerCell = document.createElement('th');
-  headerCell.textContent = 'Locations';
-  table.appendChild(headerCell);
+  table.append(tableRow);
+}
 
-  for (var i = 0; i < hoursOpen.length; i++ ) {
+//////////////Prototypes
 
-    var thElement = document.createElement('th');
-    thElement.textContent = hoursOpen[i];
-    table.appendChild(thElement);
+Stores.prototype.calcRandomNumber = function() {
+  for(var i = 0; i < hoursOpen.length; i++) {
+    this.randomNumArray.push(Math.floor( Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
 
   }
-  var totalRow = document.createElement('th');
-  totalRow.textContent = 'Totals';
-  table.appendChild(totalRow);
+}
+
+Stores.prototype.calcCookieSales = function() {
+  this.calcRandomNumber();
+  for(var i = 0; i < hoursOpen.length; i++) {
+    this.cookiesSold.push(Math.ceil(this.randomNumArray[i] * this.avgCookieSale));
+    this.totalCookieSales += this.cookiesSold[i];
+  }
+}
+
+Stores.prototype.render = function() {
+  var storesTr = document.createElement('tr');
 
   var storeList = document.createElement('tr');
-  storeList.textContent = name;
-  table.appendChild(storeList);
+  storeList.textContent = this.name;
+  storesTr.appendChild(storeList);
+  table.append(storesTr);
+
+//***************writing data in cells***************************
+  for(var i = 0; i < hoursOpen.length; i++) {
+
+    var hourlyDataCells = document.createElement('td');
+    hourlyDataCells.textContent = this.cookiesSold[i];
+    storesTr.appendChild(hourlyDataCells);
+  }
+  //Store names left side of table***************************
+  var lastColumnTotals = document.createElement('tr');
+  lastColumnTotals.textContent = this.totalCookieSales;
+  storesTr.appendChild(lastColumnTotals);
+}
+
+function makeHeaderRows() {
+  buildRow('<th></th>', hoursOpen, 'Locations');
 };
 
 function footerRows () {
@@ -118,20 +109,27 @@ function footerRows () {
   tdEl = document.createElement('td');
   tdEl.textContent = endTotal;
   trEl.appendChild(tdEl);
-  table.appendChild(trEl);
+  table.append(trEl);
   // event.preventDefault();
 
-};
+}
 
+function initiateProgram() {
+  makeHeaderRows();
 
+  new Stores('Alki', 2, 26, 2.6);
+  new Stores('CapHill', 20, 38, 2.3);
+  new Stores('SeaCenter', 11, 38, 3.7);
+  new Stores('SeaTacAP', 3, 24, 1.2);
+  new Stores('FirstandPike', 23, 65, 6.3);
 
+  for (var i = 0; i < myStores.length; i++) {
+    myStores[i].render();
+  }
 
+  footerRows();
+}
 
-
-var formData = document.getElementById('userinput');
-
-footerRows();
-formData.addEventListener('submit', inputNewUserData);
 function inputNewUserData (event) {
   event.preventDefault();
 
@@ -144,12 +142,18 @@ function inputNewUserData (event) {
   var newLocation = new Stores (valueForName,valueForMinCust, valueForMaxCust, valueForavgCookieSales );
   table.innerHTML = '';
 
-
-
-
   for (var i = 0; i < myStores.length; i++) {
     myStores[i].render();
   }
   footerRows();
-};//***************end of inputNewUserData ******************
+};
+
+////////////////Execution Code////////////////////////////////////////
+
+initiateProgram();
+formData.addEventListener('submit', inputNewUserData);
+
+inputs.val('I am only read');
+
+inputs.attr('readonly', true);
 //*******************Validations********************************
